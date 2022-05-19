91c76855c15aa2f48b7a5e528b064d76c507a932
Nestert <kindnessbel@mail.ru>
2022-05-19 22:04:22 +0300

    Add HTML page, mnist recognizer with Flask

--- mnist.py
+++ mnist.py
@@ -24,7 +24,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
     optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
 
 model.fit(x_train, y_train, batch_size=128, 
-    epochs=12, verbose=1, validation_data=(x_test, y_test))
+    epochs=100, verbose=1, validation_data=(x_test, y_test))
 
 score = model.evaluate(x_test, y_test)
 
